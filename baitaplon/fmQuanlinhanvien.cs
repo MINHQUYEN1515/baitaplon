f2dd381e41f67f1c9d287a0a6d9e4247d8a54798
luacute1010 <150404186+luacute1010@users.noreply.github.com>
2023-11-12 20:21:47 +0700

    Update fmQuanlinhanvien.cs
    phuong

--- baitaplon/fmQuanlinhanvien.cs
+++ baitaplon/fmQuanlinhanvien.cs
@@ -17,14 +17,41 @@ namespace baitaplon
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void btnThem_Click(object sender, EventArgs e)
         {
-
+            clear();
+            settrue();
+            btnLuu.Enabled = true;
+            btnThem.Enabled = false;
+            themmoi = "themmoi";
         }
 
-        private void label4_Click(object sender, EventArgs e)
+                private void btnSua_Click(object sender, EventArgs e)
         {
+            themmoi = "sua";
+            settrue();
+            btnLuu.Enabled = true;
+        }
 
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (Database.SqlConnection.State == ConnectionState.Closed)
+            {
+                Database.SqlConnection.Open();
+                DialogResult chon = MessageBox.Show("Bạn có muốn xóa không", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (chon == DialogResult.Yes)
+                {
+                    string sql = "delete from HOTENNHANVIEN Where MANHANVIEN = N'" + txtManv.Text + "'";
+                    SqlCommand cmd = new SqlCommand(sql, Database.SqlConnection);
+                    cmd.ExecuteNonQuery();
+                    Refresh();
+                    Load_DataGridView();
+                    Database.SqlConnection.Close();
+                    clear();
+                }
+                else MessageBox.Show("Chọn thông tin cần xóa", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            Database.SqlConnection.Close();
         }
 
         private void fmQuanlinhanvien_Load(object sender, EventArgs e)
